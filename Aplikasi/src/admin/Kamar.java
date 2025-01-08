4aba0023b4c056e1eb5bfc1be8f7a31d29b24be3
Muhammad Fajar <fajardj02@gmail.com>
2025-01-08 07:00:39 +0700

    mengubah tampil data di class kamar menjadi menggunakan tabel

--- Aplikasi/src/admin/Kamar.java
+++ Aplikasi/src/admin/Kamar.java
@@ -3,62 +3,67 @@ package admin;
 import javax.swing.*;
 import java.awt.*;
 import java.sql.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.table.DefaultTableModel;
 
 public class Kamar {
     public static Connection con;
     public static Statement stm;
-    public static void main(String args[]){
+
+    public static void main(String args[]) {
         // Membuat frame utama
         JFrame frame = new JFrame("Home Aplikasi Kosan");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500, 400);
+        frame.setSize(800, 600);
         frame.setLayout(new BorderLayout());
 
         // Panel untuk konten utama
         JPanel panel = new JPanel();
-        panel.setLayout(new GridBagLayout());
-       
-        DefaultListModel<String> roomListModel = new DefaultListModel<>();
-        
+        panel.setLayout(new BorderLayout());
+
+        // Membuat model tabel
+        String[] columnNames = {"ID Kamar", "No. Kamar", "Harga", "Jenis Kamar", "Ketersediaan"};
+        DefaultTableModel tableModel = new DefaultTableModel(columnNames, 0);
+        JTable table = new JTable(tableModel);
+
         try {
-            String url ="jdbc:mysql://localhost/sikosan_db";
-            String user="root";
-            String pass="";
+            String url = "jdbc:mysql://localhost/sikosan_db";
+            String user = "root";
+            String pass = "";
             Class.forName("com.mysql.jdbc.Driver");
-            con =DriverManager.getConnection(url,user,pass);
+            con = DriverManager.getConnection(url, user, pass);
             stm = con.createStatement();
             //System.out.println("koneksi berhasil;");
-            
+
             // Query untuk mendapatkan data kamar
             String query = "SELECT * FROM kamar";
             ResultSet resultSet = stm.executeQuery(query);
 
-            // Mengisi list kamar dengan data dari database
+            // Mengisi tabel dengan data dari database
             while (resultSet.next()) {
-                String roomInfo = resultSet.getInt("idKamar") + "  " + resultSet.getString("noKamar") + "  " + resultSet.getInt("harga") + "  " + resultSet.getString("jenisKamar") + "  " + resultSet.getString("ketersediaan");
-                roomListModel.addElement(roomInfo);
+                int idKamar = resultSet.getInt("idKamar");
+                String noKamar = resultSet.getString("noKamar");
+                int harga = resultSet.getInt("harga");
+                String jenisKamar = resultSet.getString("jenisKamar");
+                String ketersediaan = resultSet.getString("ketersediaan");
+
+                Object[] row = {idKamar, noKamar, harga, jenisKamar, ketersediaan};
+                tableModel.addRow(row);
             }
 
             // Menutup koneksi
             resultSet.close();
             stm.close();
             con.close();
-            
+
         } catch (Exception e) {
-            System.err.println("koneksi gagal" +e.getMessage());
+            System.err.println("Koneksi gagal: " + e.getMessage());
         }
-        
-        JList<String> roomList = new JList<>(roomListModel);
-        roomList.setFont(new Font("Arial", Font.PLAIN, 14));
-        JScrollPane scrollPane = new JScrollPane(roomList);
-        scrollPane.setPreferredSize(new Dimension(300, 150));
 
-        // Menambahkan tombol dan list ke panel
-        panel.add(scrollPane);
+        // Menambahkan tabel ke panel dengan JScrollPane
+        JScrollPane scrollPane = new JScrollPane(table);
+        panel.add(scrollPane, BorderLayout.CENTER);
 
-        // Tambahkan panel ke tengah frame
+        // Tambahkan panel ke frame
         frame.add(panel, BorderLayout.CENTER);
 
         // Atur frame agar terlihat di tengah layar
