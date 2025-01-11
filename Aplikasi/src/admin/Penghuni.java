0205e8a20b5b16cdf7b2542b182112f84456d7da
Muhammad Fajar <fajardj02@gmail.com>
2025-01-11 07:30:03 +0700

    mengedit pegawai dan menambahkan class Penghuni

--- Aplikasi/src/admin/Penghuni.java
+++ Aplikasi/src/admin/Penghuni.java
@@ -1,11 +1,18 @@
 package admin;
 
-import javax.swing.*;
-import java.awt.*;
-import java.sql.*;
+import java.awt.BorderLayout;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
-public class Pegawai {
+public class Penghuni {
     public static Connection con;
     public static Statement stm;
 
@@ -21,7 +28,7 @@ public class Pegawai {
         panel.setLayout(new BorderLayout());
 
         // Membuat model tabel
-        String[] columnNames = {"ID Pegawai", "Nama Pegawai", "No. Handphone", "Email"};
+        String[] columnNames = {"ID Penghuni", "Nama Penghuni", "No. Handphone", "Email"};
         DefaultTableModel tableModel = new DefaultTableModel(columnNames, 0);
         JTable table = new JTable(tableModel);
 
@@ -35,17 +42,17 @@ public class Pegawai {
             //System.out.println("koneksi berhasil;");
 
             // Query untuk mendapatkan data kamar
-            String query = "SELECT * FROM pegawai";
+            String query = "SELECT * FROM penghuni";
             // Mengisi tabel dengan data dari database
             try (ResultSet resultSet = stm.executeQuery(query)) {
                 // Mengisi tabel dengan data dari database
                 while (resultSet.next()) {
-                    int idPegawai = resultSet.getInt("IdPegawai");
+                    int idPenghuni = resultSet.getInt("IdPenghuni");
                     String nama = resultSet.getString("nama");
                     String noHp = resultSet.getString("noHp");
                     String email = resultSet.getString("Email");
                     
-                    Object[] row = {idPegawai, nama, noHp, email};
+                    Object[] row = {idPenghuni, nama, noHp, email};
                     tableModel.addRow(row);
                 }
                 // Menutup koneksi
